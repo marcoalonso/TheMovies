669ce3015914bcd7decf2cad91e2431092c48c55
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-06-21 00:00:02 -0600

    Getting Started

---
+++ TheMovies/TheMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMoviesApp.swift
+//  TheMovies
+//
+//  Created by Marco Alonso Rodriguez on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TheMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
