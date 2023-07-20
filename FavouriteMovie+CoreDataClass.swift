66ad5a8e2221376a25b9958508824c508b00407d
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-07-19 23:14:26 -0600

    Datamodel added

---
+++ FavouriteMovie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  FavouriteMovie+CoreDataClass.swift
+//  TheMovies
+//
+//  Created by Marco Alonso Rodriguez on 19/07/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(FavouriteMovie)
+public class FavouriteMovie: NSManagedObject {
+
+}
---
+++ FavouriteMovie+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  FavouriteMovie+CoreDataProperties.swift
+//  TheMovies
+//
+//  Created by Marco Alonso Rodriguez on 19/07/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension FavouriteMovie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<FavouriteMovie> {
+        return NSFetchRequest<FavouriteMovie>(entityName: "FavouriteMovie")
+    }
+
+    @NSManaged public var id: Int64
+    @NSManaged public var fecha: String?
+    @NSManaged public var poster: Data?
+    @NSManaged public var titulo: String?
+    @NSManaged public var descripcion: String?
+
+}
+
+extension FavouriteMovie : Identifiable {
+
+}
